2ab06eae6a9ec526802fd25e9a2e5beb899595f1
Maulvi Inayat Ali <maulvi67@gmail.com>
2018-06-30 00:35:48 +0700

    integrated metronic template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Chuppy</title>
+</head>
+<body class="page-md page-header-fixed page-quick-sidebar-over-content page-sidebar-closed-hide-logo ">
+
+    @include('layouts.header')
+<div class="page-container">
+    @include('layouts.sidebar')
+    @yield('content')
+</div>
+    @include('layouts.footer')
+    @yield('js')
+    
+</body>
+</html>
