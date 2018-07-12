77a0a560853be42a42c9aaacbbb84105402ff53d
Maulvi Inayat Ali <maulvi67@gmail.com>
2018-07-12 11:23:44 +0700

    add - header -footer -sidebar

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="page-footer">
         <div class="page-footer-inner">
-             2014 &copy; Metronic by keenthemes.
+             2018 &copy; Chuppy by DarkInnocent.
         </div>
         <div class="scroll-to-top">
             <i class="icon-arrow-up"></i>
